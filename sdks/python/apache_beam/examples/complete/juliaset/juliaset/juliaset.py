c4975c3eeef29fc0664e57debfc9498dd8f4e7a3
a-satyateja <30695365+a-satyateja@users.noreply.github.com>
2019-06-25 00:25:26 +0530

    c

--- sdks/python/apache_beam/examples/complete/juliaset/juliaset/juliaset.py
+++ sdks/python/apache_beam/examples/complete/juliaset/juliaset/juliaset.py
@@ -15,8 +15,10 @@
 # limitations under the License.
 #
 
+"""A word-counting workflow."""
 
 from __future__ import absolute_import
+import argparse
 import logging
 import os
 
@@ -24,54 +26,55 @@ import apache_beam as beam
 from PIL import Image
 from apache_beam.io.gcp import gcsio
 from apache_beam.io.fileio import MatchFiles, ReadMatches
-from apache_beam.io.gcp.gcsio import GcsIO
+from apache_beam.metrics.metric import MetricsFilter
 from apache_beam.options.pipeline_options import PipelineOptions
+from apache_beam.options.pipeline_options import SetupOptions
 from io import BytesIO
 from apache_beam.io import filesystems
-from apache_beam.options.value_provider import StaticValueProvider
 
 
 class WordExtractingDoFn(beam.DoFn):
-    def process(self, element):
-        # logging.info('element found was', element)
-        # print("reads element ::")
-        # print(element)
-        # head, file_name = os.path.split(os.path.splitext(element)[0])
-        gcsio_obj = gcsio.GcsIO()  # type: GcsIO
-        # bufferImg = gcsio_obj.open(element, 'r').read()
-        # image = Image.open(BytesIO(bufferImg))
-        # writer = filesystems.FileSystems.create(head + "/" + file_name + ".png", "image/png")
-        # b = BytesIO()
-        # image.save(b, format="png")
-        # contents = b.getvalue()
-        # writer.write(contents)
-        # writer.close()
-        gcsio_obj.delete(element)
-        # b.close()
 
+    def process(self, element):
+        print("reads element ::")
+        print(element)
+        head, file_name = os.path.split(os.path.splitext(element)[0])
+        gcsio_obj = gcsio.GcsIO()
+        bufferImg = gcsio_obj.open(element, 'r').read()
+        image = Image.open(BytesIO(bufferImg))
+        writer = filesystems.FileSystems.create(head + "/" + file_name + ".png", "image/png")
+        b = BytesIO()
+        image.save(b, format="png")
+        contents = b.getvalue()
+        writer.write(contents)
+        writer.close()
+        gcsio_obj.delete(element)
+        b.close()
 
 def run(argv=None):
+    """Main entry point; defines and runs the wordcount pipeline."""
+
     class WordcountOptions(PipelineOptions):
         @classmethod
         def _add_argparse_args(cls, parser):
-            parser.add_value_provider_argument('--input', type=str,
-                                               default='gs://unzip-testing/unzip_nested/US10294769-20190521/*.TIF')
-
+            parser.add_argument(
+                '--input',
+                default='gs://dataflow-samples/shakespeare/kinglear.txt',
+                help='Path of the file to read from')
     pipeline_options = PipelineOptions(argv)
-    # p = beam.Pipeline(options=pipeline_options)
+    p = beam.Pipeline(options=pipeline_options)
     wordcount_options = pipeline_options.view_as(WordcountOptions)
-    with beam.Pipeline(options=pipeline_options) as p:
-        files = (p | 'files' >> MatchFiles(StaticValueProvider(str, 'gs://unzip-testing/unzip_nested/US10294769-20190521/*.TIF'))
-                 | 'read-matches' >> ReadMatches()
-                 )
-        files_and_contents = (files
-                              | 'read' >> beam.Map(lambda x: x.metadata.path))
-        counts = (files_and_contents
-                  | 'read-1' >> (beam.ParDo(WordExtractingDoFn()))
-                  )
-        result = p.run()
-        result.wait_until_finish()
-
+    files = (p | 'files' >> MatchFiles(wordcount_options.input)
+             | 'read-matches' >> ReadMatches()
+             )
+    files_and_contents = (files
+                          |'read' >> beam.Map(lambda x: x.metadata.path,
+                                              ))
+    counts = (files_and_contents
+              | 'read-1' >> (beam.ParDo(WordExtractingDoFn()))
+              )
+    result = p.run()
+    result.wait_until_finish()
 
 if __name__ == '__main__':
     logging.getLogger().setLevel(logging.INFO)
